04be8496b8a6686b5c6285db7d9e5daf2acc0c8f
jmenglis <josh@joshenglish.com>
2016-05-10 08:29:58 -0600

    Changing to Materialize, adding some gulptasks to compile SCSS to CSS, changed out jQuery and added React

---
+++ BreweryApplication/gulpfile.js
@@ -0,0 +1,19 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('./Content/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(function (f) {
+        return f.base;
+    }))
+});
+
+gulp.task('default', ['sass'], function () {
+    gulp.watch('./Content/**/*.scss', ['sass']);
+});
